d6a222df3df7d9d6410e997c22f5f8258e382ec1
Yuceflpb <50379011+Yuceflpb@users.noreply.github.com>
2019-05-15 12:08:26 +0200

    est

--- tools.h
+++ tools.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include "define.h"
 
+
 struct Coord 
 {
 	double x;
@@ -30,11 +31,23 @@ Coord indice_en_coord(Carre c, int nbCell);
 bool supp_cercles(Rond co1, Rond co2, int nbcell);
 
 bool supp_cercle_carre(Rond co1, Carre ca1, int nbcell); 
-
+///zesssss
 double dist_deux_points(Coord p1, Coord p2);
 
 Coord chgmnt_coord(Coord pt_original);
 
 double tps_angle(int compteur);
+
+double pente_chemin(Coord j1, Coord j2);
+
+double calcul_b(Coord predateur, double pente);
+
+double fct_math(double angle, double x, double b);
+
+bool carre_dans_zone(Rond p1, Rond p2, Coord carre, int nbCell);
+
+double fonct_maths(double x, double pente, double b);
+
+bool sup_rect_obst(Coord obstacle, Rond j1, Rond j2, int nbCell);
 
 #endif
